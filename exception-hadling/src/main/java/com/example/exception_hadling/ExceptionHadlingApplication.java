b7f108922543304d2968a362996ed8ca72f4ec46
laroseikitama <laroseikitama@gmail.com>
2025-01-16 10:17:13 +0000

    docs:configure the application.properties file

---
+++ exception-hadling/src/main/java/com/example/exception_hadling/ExceptionHadlingApplication.java
@@ -0,0 +1,13 @@
+package com.example.exception_hadling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionHadlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionHadlingApplication.class, args);
+	}
+
+}
---
+++ exception-hadling/src/test/java/com/example/exception_hadling/ExceptionHadlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exception_hadling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionHadlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
